8435cf75d8bd52289de3d5a41e8a0711ae58f09f
OniON Igris <thanhhmgcs210383@fpt.edu.vn>
2023-05-27 11:59:52 +0700

    da chinh sua

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const express = require('http')
-const app = http()
+const http = require ( 'http' );
+const hostname = 'localhost' ;
+const port = 3000 ;
+const server = http.createServer((req, res) => {
+	res.statusCode = 200 ;
+	res.setHeader( 'Content-Type' , 'text/plain' );
+	res.end( 'Hello World ' );
+});
 
-app.get('/', (req, res) => {
-res.send('Hello, World!')
-})
-
-app.listen(3000, () => {
-console.log('Server started on port 3000')
-})
+server.listen(port, hostname, () => {
+	console .log( `Server running at http://${hostname}:${port}/` );
+});
